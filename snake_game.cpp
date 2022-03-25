ed3213820f6243236e83a5c217c7e565a8550b51
Jack <jackstephens93@hotmail.co.uk>
2022-03-25 19:03:56 +0000

    on to last part

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,20 +1,126 @@
-// snake_game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <conio.h>
+
+using namespace std;
+bool gameOver;
+
+// map dimensions
+const int width = 20;
+const int height = 20;
+
+// head position
+int x, y, fruitX, fruitY, score;
+
+enum eDirection{STOP=0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    
+    // so that snake will start in centre of the map
+    x = width / 2;
+    y = height / 2;
+
+    // randomly distribute fruit throughout map
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+
+    score = 0;
+
+}
+
+void Draw() {
+    system("cls");
+    
+    // define map exterior using '#'
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+
+    // print the map interior (like printing a matrix)
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0)
+                cout << "#";
+            
+            if (i == y && j == x)
+                cout << "O";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+                cout << " ";
+
+            if (j == width - 1)
+                cout << "#";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 2; i++)
+        cout << "#";
+    cout << endl;
+    cout << "Score: " << score << endl;
+}
+
+void Input() {
+    if (_kbhit()) {
+        switch (_getch()) {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+void Logic() {
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+    if (x > width || x < 0 || y > height || y < 0)
+        gameOver = true;
+    if (x == fruitX && y == fruitY) {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+    }
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
